77666438d2b098d4f4b1e57ab9938ecd2eb87104
Michał Górski <michal.gorski.pl@gmail.com>
2017-07-03 07:37:29 +0200

    Update presentation

--- Code/src/test/java/pl/thelizardproject/sda/medium/Exercise5.java
+++ Code/src/test/java/pl/thelizardproject/sda/medium/Exercise5.java
@@ -6,6 +6,7 @@ import pl.thelizardproject.sda.medium.captain.Captain;
 import pl.thelizardproject.sda.medium.ship.Boat;
 import pl.thelizardproject.sda.medium.ship.ShipType;
 import pl.thelizardproject.sda.medium.ship.TransatlanticShip;
+import pl.thelizardproject.sda.medium.ship.strategy.BoatControlStrategy;
 import pl.thelizardproject.sda.medium.ship.strategy.TransatlanticShipControlStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,7 +22,7 @@ public class Exercise5 {
     }
 
     @Test
-    public void testSailingWithKnownShip() throws Exception {
+    public void testSailingWithKnownTransatlanticShip() throws Exception {
         String expectedSailStyle = "I can sail with Transatlantic Ship with a transatlantic ship style!";
         captain.setShip(new TransatlanticShip());
         captain.learn(ShipType.Transatlantic, new TransatlanticShipControlStrategy());
@@ -33,7 +34,31 @@ public class Exercise5 {
     }
 
     @Test
-    public void testSailingWithUnknownShip() throws Exception {
+    public void testSailingWithKnownBoat() throws Exception {
+        String expectedSailStyle = "I can sail with Boat with a boat style!";
+        captain.setShip(new Boat());
+        captain.learn(ShipType.Boat, new BoatControlStrategy());
+
+        String sailStyle = captain.sail();
+
+        assertThat(sailStyle)
+                .isEqualTo(expectedSailStyle);
+    }
+
+    @Test
+    public void testSailingWithUnknownTransatlanticShip() throws Exception {
+        String expectedSailStyle = "I can't sail with Transatlantic Ship!";
+        captain.setShip(new TransatlanticShip());
+        captain.learn(ShipType.Boat, new BoatControlStrategy());
+
+        String sailStyle = captain.sail();
+
+        assertThat(sailStyle)
+                .isEqualTo(expectedSailStyle);
+    }
+
+    @Test
+    public void testSailingWithUnknownBoat() throws Exception {
         String expectedSailStyle = "I can't sail with Boat!";
         captain.setShip(new Boat());
         captain.learn(ShipType.Transatlantic, new TransatlanticShipControlStrategy());
